8c4918821f0e3f0cbf6906b247369450b2ff225c
Abdallah-Abu-Karaki <abdallahabukaraki10@gmail.com>
2020-11-11 16:17:08 +0200

    this is template to review bootstrap

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+
+
+var brandColor = document.getElementById('narrow');
+    
+   brandColor.firstElementChild.style = 
+
+   "color:#d2aadb; padding-top:20px; font-size:30px";
+
+var textSectonNarrow = document.getElementById('narrow');
+
+    textSectonNarrow.childNodes[3].style = 
+    
+    "color: #777; font-size:18px; padding-top:20px; line-height:1.8; padding:0px 30px; " ;
